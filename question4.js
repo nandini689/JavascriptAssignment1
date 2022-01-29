fb27c17193ec335f423fd6413aecbf7435196555
Nandini Gupta <65002182+nandini689@users.noreply.github.com>
2022-01-29 18:07:10 +0530

    Add files via upload

---
+++ question4.js
@@ -0,0 +1,11 @@
+const number = 0;
+
+if (number >= 0) {
+    if (number == 0) {
+        console.log("The number is zero");
+    } else {
+        console.log("The number is positive");
+    }
+} else {
+    console.log("The number is negative");
+}
